6e23881aaddc1777145cbf80bb7aee2ecad6f6ad
ryuujo1573 <ryuujo1573@users.noreply.github.com>
2022-07-12 02:19:34 +0000

    deploy: 6e5d58519608e5dc2aa7722ab775b7d312d663c9

---
+++ assets/404.3a4e4cc4.js
@@ -0,0 +1,1 @@
+import"./index.ffc48c79.js";import{B as o}from"./button.f91f050b.js";import{R as s}from"./index.40cfabd0.js";import{k as t,m as e}from"./react-venders.56ac5b88.js";const p=()=>t(s,{status:"404",title:"404",subTitle:"Sorry, the page you visited does not exist.",extra:t(o,{type:"primary",onClick:()=>e.push("/"),children:"Back Home"})});export{p as default};
---
+++ assets/RootLayout.3919645a.js
@@ -0,0 +1,1 @@
+import{k as o,F as a}from"./react-venders.56ac5b88.js";const s=({children:t})=>o(a,{children:t});export{s as default};
